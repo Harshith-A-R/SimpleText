ecce075c9d0cb2f135e7f52cb7189c57e9c75246
Harshith-A-R <100902368+Harshith-A-R@users.noreply.github.com>
2022-03-04 12:23:46 +0530

    if condition added

--- SampleJava.java
+++ SampleJava.java
@@ -3,6 +3,16 @@ class sample{
 public static void main(String[] args) {
   System.out.println("Hello Welcome to Git Hub");
    System.out.println("Welcome Harshith");
+  int a,b,c;
+  a=10;
+  b=12;
+  c=a+b;
+  if(c>30){
+    System.out.println(c);
+  }
+  else{
+    System.out.println("Welcome Harshith");
+  }
   
   }
 }
